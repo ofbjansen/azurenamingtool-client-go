fa90ed8ebdeab617031ce2cbc006608e971b346e
onnojansen <onno.jansen@proact.nl>
2025-07-31 22:54:28 +0200

    resourcetypes

--- models.go
+++ models.go
@@ -41,3 +41,27 @@ type Ingredient struct {
 	Quantity int    `json:"quantity"`
 	Unit     string `json:"unit"`
 }
+
+// ResourceTypes -
+type ResourceTypes struct {
+	ID                           int    `json:"id"`
+	Name                         string `json:"name"`
+	resource                     string `json:"resource"`
+	optional                     string `json:"optional"`
+	exclude                      string `json:"exclude"`
+	property                     string `json:"property"`
+	ShortName                    string `json:"short_name"`
+	scope                        string `json:"scope"`
+	lengthMin                    string `json:"length_min"`
+	lengthMax                    string `json:"length_max"`
+	validText                    string `json:"valid_text"`
+	invalidText                  string `json:"invalid_text"`
+	invalidCharacters            string `json:"invalid_characters"`
+	invalidCharactersStart       string `json:"invalid_characters_start"`
+	invalidCharactersEnd         string `json:"invalid_characters_end"`
+	invalidCharactersConsecutive string `json:"invalid_characters_consecutive"`
+	regx                         string `json:"regx"`
+	staticValues                 string `json:"static_values"`
+	enabled                      bool   `json:"enabled"`
+	applyDelimiter               bool   `json:"apply_delimiter"`
+}
